a53839d4289a8e60edfba966f9d5a4113c8aae4e
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2023-05-10 14:00:18 -0400

    Create exe3.c

---
+++ Lista de Exercicios 08.05/exe3.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include <math.h>
+
+int main()
+
+{ 
+
+int b,a;
+
+int vetpar[12];
+
+int vetimpar[12];
+
+for(b=0; b<12; b++)
+
+{
+
+printf(" digite um numero: ");
+
+scanf("%d", &a);
+
+if( a % 2 == 0)
+
+{
+
+vetpar[b] = a;
+
+}
+
+if ( a % 2 == 1)
+
+{
+
+vetimpar[b] = a;
+
+}
+
+}
+
+for(b=0; b<12; b++)
+
+{
+
+printf(" \n os numeros impares são: %d ", vetimpar[b]); }
+
+for(b=0; b<12; b++)
+
+{
+
+printf(" \n os numeros pares são: %d ", vetpar[b]); 
+
+}
+
+}
